a5932076423e1c4ed79a01e1fb7c8ff90a010dff
Vladislav Osada <Osada.0339@gmail.com>
2023-04-09 14:02:17 +0300

    Hello commit

--- src/First.java
+++ src/First.java
@@ -1,5 +1,8 @@
 public class First {
+    public static String sayHello(){
+        return "Hello world";
+    }
     public static void main(String[] args) {
-
+        System.out.println(First.sayHello());
     }
 }
